20e6a2f7ae3e4b697698321381307297c7bbc97e
Damien Toledo <damien@nirmata.com>
2023-09-16 11:15:49 -0700

    Update requests-utils.js

--- tests/utils/requests-utils.js
+++ tests/utils/requests-utils.js
@@ -5,19 +5,8 @@ export const getNirmataUrl = () => {
   return `https://${__ENV.NIRMATA_URL}`;
 }
 
-export const getApiToken = (role = 'admin') => {
-  const token = '';
-  switch (role) {
-    case "admin":
-      return `${__ENV.NIRMATA_ADMIN_API_TOKEN}`;
-    case "platform":
-      return `${__ENV.NIRMATA_PLATFORM_API_TOKEN}`;
-    case "devops":
-      return `${__ENV.NIRMATA_DEVOPS_API_TOKEN}`;
-    default:
-      console.log("Invalid role "+role);
-      return null;
-  }
+export const getApiToken = () => {
+  return `${__ENV.NIRMATA_API_TOKEN}`;
 }
 
 export const getHeadersWithAuth = (role = 'admin') => {
